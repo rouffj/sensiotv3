cea88ed7474d7eff6207d4a666123c7fd59ab758
Joseph Rouff <rouffj@gmail.com>
2021-06-08 10:27:27 +0200

    Add form_theme + render SignupType

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\SignupType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type as Type;
 
 /**
  * @Route("/user", name="user_")
@@ -24,6 +26,11 @@ class UserController extends AbstractController
      */
     public function register(): Response
     {
-        return $this->render('user/register.html.twig');
+        $signupForm = $this->createForm(SignupType::class);
+        $signupForm->add('signup', Type\SubmitType::class);
+
+        return $this->render('user/register.html.twig', [
+            'signup_form' => $signupForm->createView()
+        ]);
     }
 }
